fe6fa6e7dd55c21c04b1151a4c1ed5813b7ed39b
Seong-Jun1525 <sjsj123455@naver.com>
2023-12-19 12:23:18 +0900

    CH01 commit

---
+++ CH01/ex1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+	printf("Hello World\n");
+	printf("Hello World");
+	printf("\n");
+
+	// 간단한 계산 해보기
+	printf("1 + 1 = %d\n", 1 + 1);
+	printf("2 - 2 = %d\n", 2 - 2);
+	printf("3 * 3 = %d\n", 3 * 3);
+	printf("4 / 4 = %d\n", 4 / 4);
+	printf("4 %% 4 = %d", 4 % 4);
+	return 0;
+}
